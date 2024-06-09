4841327d6fc89d5018d59d10fd160fb49718a396
Bruce Wayne <bruce@wayne-enterprises.tech>
2024-06-09 05:33:28 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import antigravity
 
-SEED=ed5ed557-7910-44bc-b44f-7f3d5b2d36cb
+CERT="""-----BEGIN DSA PRIVATE KEY-----
+MIIBugIBAAKBgQCnHg7G4DQrQIiXi1pU4U6H66VeH5A6XKEG0w/OPqn9dQUGvu5M
+Ve9f2G8rqQtBi+Azpf6lBLsLQD/WervRZuT/rm7P5QHrtdnssMTqm+33o7yB7WwY
+02ueZm+qC3yFgTvFesuXEkkyOL0/7fuR+kIwDSlmoXpjTaeFLw2G0E32OwIVAOzF
+6NE+5h/y7o7TTZ5wJzfO33p/AoGAU/J0nChDVm1C7Bn4O/iSrHSQ+QHL/djphlf/
+027rIs+9jINW/V89WWs7NaVK3GnygIB/uDv7AZlkitiuzMOo+8bJm1RqXzftBLw8
+41iZfeQM6m/isL72vIIAj5mJwlm+8i0/v8bpNWYEQ+HTmWLX7e1if6a4eEKgn80b
+XgCOMTMCgYB5bDcfYjGaAoj9FJkUM/Emt/q+ncbRLqahtBiicTNr2IdcfhxeWgNM
+AuPhyVMInLdEzjdKigriWUZ8F8XZZWKZ9QMrjU5mIZgDxd5oytSFgWaQiVabrwA/
+JC3lD3MBaoBYSWgPgd8DQ02RSNR/9Q7CRz76XsfmZ6GyKPbkLqmbAQIUSBsai/pF
+Jq8Pmf8vx8X8Ztr1wm8=
+-----END DSA PRIVATE KEY-----
+"""
 
 def main():
     antigravity.fly()
